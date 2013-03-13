f43160c84609e60f5d32dd338d0fcfeb5590c254
krizzmp <krizzmp@gmail.com>
2013-03-13 13:24:05 +0100

    making calc-solve.py able to beal with normal calculations as well as solving

--- calc-solve.py
+++ calc-solve.py
@@ -9,7 +9,7 @@ reserved = {
 }
 tokens = [
     'NAME','NUMBER',
-    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
+    'PLUS','MINUS','TIMES','DIVIDE','EQUALS','EQ',
     'LPAREN','RPAREN',
     'COMMA',
     ]+ list(reserved.values())
@@ -24,6 +24,7 @@ t_EQUALS  = r'='
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_COMMA   = r','
+t_EQ      = r'=='
 
 def t_NAME(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
@@ -63,70 +64,87 @@ precedence = (
     )
 
 # dictionary of names
-names = []
+names = {}
 result ={ }
 
+def p_error(t):
+    print("Syntax error at '%s'" % t.value)
+
 def p_solved(t):
-    'solved : SOLVE LPAREN eq EQUALS eq COMMA NAME RPAREN'
+    'statement : SOLVE LPAREN expression EQ expression COMMA NAME RPAREN'
+    print 'i was called'
     result[0]=t[7]
-    result[1]=sympy.solve(t[3]+'-'+t[5],sympy.symbols(t[7]))
+    result[1]=sympy.solve(t[3]+'-'+t[5],t[7])
+    
 
+def p_statement_assign(t):
+    'statement : NAME EQUALS expression'
+    print t[3]
+    names[t[1]] = t[3]
+    result[1] = eval(t[3])
+    
 
-
-def p_eq_name(t):
-    'eq : NAME'
-    t[0]=sympy.symbols(t[1])
-    names.append(t[1])
-
-def p_eq_binop(t):
-    '''eq : eq PLUS eq
-          | eq MINUS eq
-          | eq TIMES eq
-          | eq DIVIDE eq'''
-    t[0]=str(t[1])+t[2]+str(t[3])
+def p_statement_expr(t):
+    'statement : expression'
+    
+    t[0] = t[1]
+    print t[0]
+    result[1] = eval(t[0])
+    
 
 def p_expression_binop(t):
     '''expression : expression PLUS expression
                   | expression MINUS expression
                   | expression TIMES expression
                   | expression DIVIDE expression'''
-    if t[2] == '+'  : t[0] = t[1] + t[3]
-    elif t[2] == '-': t[0] = t[1] - t[3]
-    elif t[2] == '*': t[0] = t[1] * t[3]
-    elif t[2] == '/': t[0] = t[1] / t[3]
-
+    if t[2] == '+'  : t[0] = str(t[1]) +'+'+ str(t[3])
+    elif t[2] == '-': t[0] = str(t[1]) +'-'+ str(t[3])
+    elif t[2] == '*': t[0] = str(t[1]) +'*'+ str(t[3])
+    elif t[2] == '/': t[0] = str(t[1]) +'/'+ str(t[3])
+    
 
 def p_expression_uminus(t):
     'expression : MINUS expression %prec UMINUS'
     t[0] = -t[2]
-def p_expression(t):
-    'eq : expression'
-    t[0] = t[1]
+    
 
 def p_expression_group(t):
     'expression : LPAREN expression RPAREN'
     t[0] = t[2]
+    
 
 def p_expression_number(t):
     'expression : NUMBER'
+    
     t[0] = t[1]
+    
 
-def p_error(t):
-    print("Syntax error at '%s'" % t.value)
+def p_expression_name(t):
+    'expression : NAME'
+    try:
+        t[0] = names[t[1]]
+    except LookupError:
+        #print("Undefined name '%s'" % t[1])
+        t[0] = t[1]
+        print t[0]
+
+
+
 
 import ply.yacc as yacc
 yacc.yacc()
 
 
 s="""
-solve(p*v=n*r*t , t)
-solve(p*g=n*r*t , t)
+solve(p*v==n*r*t , t)
+solve(p*g==n*r*t , t)
+a_d=6+7/(4+5)*6
+a_d+4
 """
 
-print [x**2 for x in range(1,9) if x%2]
 import sympy
 for line in s.split("\n"):
     if line:
         yacc.parse(line)
         print '\n',line,'->'
-        print result[0],'=',result[1][0]
+        print result[1]
